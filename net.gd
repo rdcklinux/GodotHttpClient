8f6075a06514063a7c40a70f80042c594d7d188d
Roderick Lagunas <rdcklinux@gmail.com>
2017-03-04 20:16:50 -0300

    mejora legibilidad del código

--- net.gd
+++ net.gd
@@ -5,10 +5,10 @@ var http = null
 func _ready():
 	var GodotWebClient = load("GodotWebClient.gd").GodotWebClient
 	http = GodotWebClient.new('127.0.0.1', 5000, self)
-	http.get('/get', 'get_save', {"param":"query"})
-	http.post('/save', 'post_save', {"param":"query"})
+	http.get('/get', 'get_response', {"param":"query"})
+	http.post('/save', 'post_response', {"param":"data"})
 
-func get_save(result, response_code, headers, body):
+func get_response(result, response_code, headers, body):
 	print("GET")
 	print(result)
 	print(response_code)
@@ -16,7 +16,7 @@ func get_save(result, response_code, headers, body):
 	print(body.get_string_from_utf8())
 	http.terminate()
 	
-func post_save(result, response_code, headers, body):
+func post_response(result, response_code, headers, body):
 	print("POST")
 	print(result)
 	print(response_code)
