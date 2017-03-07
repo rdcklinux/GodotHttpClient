a957ff8a6464ed32d902b90a44eea855d4962b58
Roderick Lagunas <rlagunas@fidelizador.com>
2017-03-07 12:26:08 -0300

    se implementa OAuth2 flow ClientCredentials

--- GodotNetworking.gd
+++ GodotNetworking.gd
@@ -2,15 +2,15 @@ extends Node
 
 class WebClient:
 	var host = '127.0.0.1'
-	var port = 5000
+	var port = 80
 	var use_ssl = false
-	var __headers = StringArray()
+	var __headers = StringArray([])
 	var __queue = Array()
 	var __client = null
 	var object = null
 	
 
-	func _init(host, port, object, use_ssl=false):
+	func _init(host, object, port=80, use_ssl=false):
 		self.host = host
 		self.port = port
 		self.use_ssl = use_ssl
@@ -51,22 +51,62 @@ class WebClient:
 		self.__perform_request(HTTPClient.METHOD_POST, callback, path, data)
 
 class OAuth2:
-	
+
 	class Client:
-		var host = ''
-		var port = ''
-		var basepath = ''
+		var host = '127.0.0.1'
+		var port = 80
 		var http = null
+		var use_ssl=false
+		var flow = null
+		var access_token = ''
 	
-		func _init(server, port, object, use_ssl=false):
-			self.http = WebClient.new(server, port, use_ssl, object)
+		func _init(host, flow, object, port=80, use_ssl=false):
+			self.host = host
+			self.port = port
+			self.use_ssl = use_ssl
+			self.http = WebClient.new(host, object, port, use_ssl)
+			self.flow = flow
 			
+		func request_access_token():
+			self.flow.request_access_token(self.http)
+
+		func set_access_token(token):
+		  self.access_token = token
+
+		func request(path, callback, headers=StringArray([]), data={}, method='GET'):
+			headers.append_array(['Authorization: Bearer ' + self.access_token])
+			self.http.set_headers(headers)
+			if method == 'POST':
+				self.http.post(path, callback, data)
+			elif method == 'GET':
+				self.http.get(path, callback, data)
+
+		func finish_request():
+			self.http.terminate()
+
 
 	class ClientCredentials:
 		var client_id = ''
-		var secret_id = ''
-		
-		func _init(client_id, secret_id):
+		var client_secret = ''
+		var method = 'POST'
+		var access_token_path = ''
+		var callback = null
+
+		func _init(client_id, client_secret, access_token_path, callback, method='POST'):
+			self.method = method
 			self.client_id = client_id
-			self.secret_id = secret_id
-			
+			self.client_secret = client_secret
+			self.access_token_path = access_token_path
+			self.callback = callback
+		
+		func request_access_token(http):
+			var data = {
+			  'client_id' : self.client_id,
+			  'client_secret' : self.client_secret,
+			  'grant_type': 'client_credentials'
+			}
+			if self.method == 'POST':
+				http.post(self.access_token_path, self.callback, data)
+			elif self.method == 'GET':
+				http.get(self.access_token_path, self.callback, data)
+
--- net.gd
+++ net.gd
@@ -1,11 +1,21 @@
 extends Node
 
 var http = null
+var oauth2_client = null
 
 func _ready():
-	http = Networking.WebClient.new('127.0.0.1', 5000, self)
+  #Common Request
+	http = Networking.WebClient.new('127.0.0.1', self, 5000)
 	http.get('/get', 'get_response', {"param":"query"})
 	http.post('/save', 'post_response', {"param":"data"})
+	
+	#OAuth2 Request
+	var cid = 'my_client_id'
+	var csc = 'my_client_secret'
+	var get_token_path = '/oauth/v2/token'
+	var flow = Networking.OAuth2.ClientCredentials.new(cid, csc, get_token_path, 'get_oauth2_access_token')
+	oauth2_client = Networking.OAuth2.Client.new('127.0.0.1', flow, self)
+	oauth2_client.request_access_token()
 
 func get_response(result, response_code, headers, body):
 	print("GET")
@@ -22,3 +32,18 @@ func post_response(result, response_code, headers, body):
 	print(headers)
 	print(body.get_string_from_utf8())
 	http.terminate()
+	
+func get_oauth2_access_token(result, response_code, headers, body):
+	var data = {}
+	data.parse_json(body.get_string_from_utf8())
+	oauth2_client.set_access_token(data['access_token'])
+	oauth2_client.finish_request()
+	var my_headers = StringArray(['X-Custom-Header: my_value_header'])
+	oauth2_client.request('/1.0/list/download/report_197.csv', 'get_data', my_headers)
+
+
+func get_data(result, response_code, headers, body):
+	print(body.get_string_from_utf8())
+	oauth2_client.finish_request()
+
+
